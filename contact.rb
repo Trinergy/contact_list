8ff3e7d1bfa1d96ae650e9943c07880686eb3844
Trinergy <wu.kenny1221@gmail.com>
2016-01-12 20:31:50 +0000

    change:2 got it to work, yet to refactor

--- contact.rb
+++ contact.rb
@@ -4,34 +4,77 @@ require 'csv'
 class Contact
 
   attr_accessor :name, :email
+  
 
   def initialize(name, email)
-    # TODO: Assign parameter values to instance variables.
+    @name = name
+    @email = email
+    index = CSV.read('contacts.csv').length + 1
+    contacts = File.open('contacts.csv', 'a')
+    contacts.puts "#{index}, #{name}, #{email}"
   end
+
+
 
   # Provides functionality for managing a list of Contacts in a database.
   class << self
 
     # Returns an Array of Contacts loaded from the database.
     def all
+      contacts = CSV.read('contacts.csv')
+      contacts
       # TODO: Return an Array of Contact instances made from the data in 'contacts.csv'.
     end
 
     # Creates a new contact, adding it to the database, returning the new contact.
     def create(name, email)
+      Contact.new name, email
       # TODO: Instantiate a Contact, add its data to the 'contacts.csv' file, and return it.
     end
 
     # Returns the contact with the specified id. If no contact has the id, returns nil.
     def find(id)
-      # TODO: Find the Contact in the 'contacts.csv' file with the matching id.
+      id = id.to_s
+      CSV.foreach('contacts.csv') do |row|
+        return row if row.include?(id)
+      end
     end
 
     # Returns an array of contacts who match the given term.
     def search(term)
+      term = term.to_s
+      File.open('contacts.csv').readlines.each do |line|
+        puts line if line.include?(term)
+      end
+
       # TODO: Select the Contact instances from the 'contacts.csv' file whose name or email attributes contain the search term.
     end
 
   end
 
 end
+
+# print Contact.all
+
+# print Contact.find(1)
+
+# contacts = File.open('contacts.csv', 'wb') do |file|
+#   file.readlines.each_with_index do |line, idx|
+#     puts idx
+#   end
+# end
+
+# contacts = CSV.read('contacts.csv')
+# puts contacts.length
+
+# contacts = File.open('contacts.csv', 'a')
+# contacts.puts "hello"
+
+
+# Contact.search("@gmail")
+
+# puts Contact.find(3)
+
+
+
+
--- contact_list.rb
+++ contact_list.rb
@@ -1,8 +1,54 @@
 require_relative 'contact'
 
 # Interfaces between a user and their contact list. Reads from and writes to standard I/O.
+
+
+
 class ContactList
 
   # TODO: Implement user interaction. This should be the only file where you use `puts` and `gets`.
-
+  
+  
 end
+
+puts "Here is a list of available commands: \b
+       new      - Create a new contact\b
+       list     - List all contacts\b
+       show     - Show a contact\b
+       search   - Search contacts"
+
+  if ARGV.first
+    user_input = ARGV.first
+  else
+    user_input = gets.chomp
+  end
+
+  case user_input
+  when "new"
+    puts "Please input the new contact's name: "
+    name = gets.chomp.to_s
+    puts "Please input the new contact's email: "
+    email = gets.chomp.to_s
+    Contact.create(name, email)
+    puts "Your new contact has been added to the list."
+  when "list"
+    puts Contact.all
+  when "show"
+    if ARGV.length > 1
+      id = ARGV[1]
+    else
+      ARGV.clear
+      puts "Please enter the contact's index: "
+      id = gets.chomp.to_s
+    end
+    puts Contact.find(id)
+  when "search"
+    if ARGV.length > 1
+      input = ARGV[1]
+    else
+      ARGV.clear
+      puts "Please enter the contact's name or email: "
+      input = gets.chomp.to_s
+    end
+    Contact.search(input)
+  end
