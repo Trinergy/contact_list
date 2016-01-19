f6289a820f55f5a66a19a6b3ef60cc7dbcb13d51
Trinergy <wu.kenny1221@gmail.com>
2016-01-19 22:54:24 +0000

    add ORM database w/ update and delete

--- contact.rb
+++ contact.rb
@@ -1,80 +1,129 @@
-require 'csv'
+require 'pg'
 
 # Represents a person in an address book.
 class Contact
 
-  attr_accessor :name, :email
-  
+  attr_accessor :name, :email, :id
 
-  def initialize(name, email)
-    @name = name
-    @email = email
-    index = CSV.read('contacts.csv').length + 1
-    contacts = File.open('contacts.csv', 'a')
-    contacts.puts "#{index}, #{name}, #{email}"
+  DATABASE_NAME = 'contact_list'
+
+  
+  def save
+    if persisted?
+      res = self.class.conn.exec_params('UPDATE contacts SET name = $2, email = $3 WHERE id = $1::int;', [id, name, email])
+    else
+      res = self.class.conn.exec_params('INSERT INTO contacts (name, email) VALUES ($1, $2) RETURNING id', [name, email])
+      self.id = res[0]['id']
+    end
   end
+
+  #FORMATTING - when typing puts, it first converts to string, so you format it overwriting to_s
+  def to_s
+    "id: #{id}\nname: #{name}\nemail: #{email}\n"
+  end
+
+  def persisted?
+    !id.nil?
+  end
+
+  def destroy
+    res = self.class.conn.exec_params('DELETE FROM contacts WHERE id = $1::int;', [id])
+  end
+
 
 
 
   # Provides functionality for managing a list of Contacts in a database.
   class << self
 
-    # Returns an Array of Contacts loaded from the database.
+    def conn
+      PG::Connection.open(dbname: DATABASE_NAME)
+    end
+
     def all
-      contacts = CSV.read('contacts.csv')
-      contacts
-      # TODO: Return an Array of Contact instances made from the data in 'contacts.csv'.
+      res = conn.exec_params('SELECT * FROM contacts;')
+      res.each do |contact|
+        puts contact
+      end
     end
 
     # Creates a new contact, adding it to the database, returning the new contact.
-    def create(name, email)
-      Contact.new name, email
-      # TODO: Instantiate a Contact, add its data to the 'contacts.csv' file, and return it.
+    def create
+      puts "Please input contact's name: "
+      name = gets.chomp
+      puts "Please input contact's email: "
+      email = gets.chomp
+      new_contact = Contact.new
+      new_contact.name = name
+      new_contact.email = email
+      new_contact.save
+      puts "You have added:"
+      puts new_contact
     end
 
     # Returns the contact with the specified id. If no contact has the id, returns nil.
     def find(id)
-      id = id.to_s
-      CSV.foreach('contacts.csv') do |row|
-        return row if row.include?(id)
-      end
+      res = conn.exec_params('SELECT * FROM contacts WHERE id = $1::int;',[id])
+      found_contact = Contact.new
+      found_contact.id = res[0]['id']
+      found_contact.name = res[0]['name']
+      found_contact.email = res[0]['email']
+      found_contact
     end
 
     # Returns an array of contacts who match the given term.
     #use select and maybe come up with a method that changes the display
-    def search(term)
-      term = term.to_s
-      File.open('contacts.csv').readlines.each do |line|
-        puts line if line.include?(term)
+    def search(input)
+      res = conn.exec_params("SELECT * FROM contacts WHERE name LIKE '%' || $1 || '%' OR email LIKE '%' || $1 || '%';", [input])
+      if res.num_tuples == 0
+        "No contacts found"
+      else
+        res.each do |contact|
+          found_contact = Contact.new
+          found_contact.id = contact['id']
+          found_contact.name = contact['name']
+          found_contact.email = contact['email']
+          puts found_contact
+        end
       end
+    end
 
-      # TODO: Select the Contact instances from the 'contacts.csv' file whose name or email attributes contain the search term.
+    def update(id)
+      the_contact = find(id)
+      puts "What is #{the_contact.name}'s new name? "
+      new_name = gets.chomp
+      puts "What will #{the_contact.name}'s new email be?\n old email:#{the_contact.email} "
+      new_email = gets.chomp
+      the_contact.name = new_name
+      the_contact.email = new_email
+      the_contact.save
+      puts "You have updated the contact's info to: "
+      puts the_contact
+    end
+
+    def delete(id)
+      the_contact = find(id)
+      name = the_contact.name
+      the_contact.destroy
+      puts "#{name} has been deleted from your contact list"
     end
 
   end
 
 end
 
-# print Contact.all
-
-# print Contact.find(1)
-
-# contacts = File.open('contacts.csv', 'wb') do |file|
-#   file.readlines.each_with_index do |line, idx|
-#     puts idx
-#   end
-# end
-
-# contacts = CSV.read('contacts.csv')
-# puts contacts.length
-
-# contacts = File.open('contacts.csv', 'a')
-# contacts.puts "hello"
 
 
-# Contact.search("@gmail")
 
-# puts Contact.find(3)
+
+
+
+
+
+
+
+
+
 
 
 
--- contact_list.rb
+++ contact_list.rb
@@ -1,21 +1,24 @@
 require_relative 'contact'
+require 'pg'
 
-# Interfaces between a user and their contact list. Reads from and writes to standard I/O.
+# # Interfaces between a user and their contact list. Reads from and writes to standard I/O.
 
 
 
-class ContactList
+# class ContactList
 
-  # TODO: Implement user interaction. This should be the only file where you use `puts` and `gets`.
+#   # TODO: Implement user interaction. This should be the only file where you use `puts` and `gets`.
   
   
-end
+# end
 
 puts "Here is a list of available commands: \b
        new      - Create a new contact\b
        list     - List all contacts\b
        show     - Show a contact\b
-       search   - Search contacts"
+       search   - Search contacts\b
+       update   - update contact\b
+       delete   - remove contact"
 
   if ARGV.first
     user_input = ARGV.first
@@ -52,4 +55,23 @@ puts "Here is a list of available commands: \b
       input = gets.chomp.to_s
     end
     Contact.search(input)
+  when "update"
+    if ARGV.length > 1
+      input = ARGV[1]
+    else
+      ARGV.clear
+      puts "Please enter the contact's id: "
+      input = gets.chomp.to_s
+    end
+    ARGV.clear
+    Contact.update(input)
+  when "delete"
+    if ARGV.length > 1
+      input = ARGV[1]
+    else
+      ARGV.clear
+      puts "Please enter the contact's id: "
+      input = gets.chomp.to_s
+    end
+    Contact.delete(input)
   end
